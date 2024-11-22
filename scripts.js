81aa931caf239773e2e03c88c1ed765725f63a49
PriyanshTomar <132385827+PriyanshTomar@users.noreply.github.com>
2024-11-23 01:23:15 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('letter-container').style.display = 'block';
+    }, 5000); // 5 seconds delay before showing the letter
+});
